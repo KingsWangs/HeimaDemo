fbc1352834dfd918bcea03fa3c1f752e004516d2
Administrator <Administrator>
2016-09-17 20:51:39 +0800

    老司机在回家

---
+++ app/src/main/java/com/example/administrator/heimademo15/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.administrator.heimademo15;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("老司机回家");
+        System.out.println("老司机回家");
+        System.out.println("老司机回家");
+        System.out.println("老司机回家");
+        System.out.println("老司机回家");
+        System.out.println("新手回家");
+        System.out.println("新手回家");
+        System.out.println("新手回家");
+    }
+}
